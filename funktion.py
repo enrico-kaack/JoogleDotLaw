f2852baae46862ae0424f152be15b642dc12b871
LukasBall <lukas.ballweg@riseup.net>
2019-04-06 11:51:36 +0200

    woertersuche

--- funktion.py
+++ funktion.py
@@ -1,20 +1,24 @@
+def einzelwortsuche(Text, Schlagwort):
+    Anzahl = -1 #da while Schleife immer mind. einmal durchlaufen wird
+    position = 0
+    #ende = len(Text)
+    while -1 != position:
+        position = Text.find(Schlagwort, position+1)  
+        Anzahl += 1
+    return Anzahl
+
 # Funktion, die ein Urteil (Suchergebnis) nach vordefinierten Schlagwörtern durchsucht
 # und so deren Wichtigkeit beurteilt
-def woertersuche (Text, Schlagwortliste):
-    ergebnismap = {}
+def woertersuche(Text, Schlagwortliste): #Text ist string, Schlagwortliste ist Dictionary
+    for key in Schlagwortliste:
+        #ursprünglicher Wichtigkeitswert wird mit Häufigkeit des Wortes multipliziert
+        Schlagwortliste[key] = Schlagwortliste[key] * einzelwortsuche(Text, key) 
+    return Schlagwortliste
 
-    #Iterieren über Schlagwortliste
-    for i in range(len(Schlagwortliste)):
-        Paar = (Schlagwortliste[i] : einzelwortsuche(Text, Schlagwortliste[i]))
-        ergebnismap.update(Paar)
-    
-    return ergebnismap
+def main():
+    Text = "hallo ich heisse Lukas und komme aus Heidelberg und der Hackathon ist in Heidelberg"
+    Schlagwortliste = {"Lukas" : 1, "Heidelberg" : 1.5, "und" : 0.5}
+    Ergebnisliste = woertersuche(Text, Schlagwortliste)
+    print(Ergebnisliste)
 
-def einzelwortsuche (Text, Schlagwort):
-    j = -1 #da while Schleife immer mind. einmal durchlaufen wird
-    position = 0
-    ende = len(Text)
-    while position != -1
-        position = Text.find(Schlagwort[,position[,ende]])  
-        j += 1
-    return j
+main()
